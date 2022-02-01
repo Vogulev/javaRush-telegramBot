74e1c716724f17d6916a0e13389fde6e5495e816
Artem.Vogulev <Artem.Vogulev@russianpost.ru>
2022-02-01 11:38:57 +0300

    JRTB-7: added the ability to delete group subscription.

--- src/main/java/com/github/vogulev/jrtb/service/GroupSubService.java
+++ src/main/java/com/github/vogulev/jrtb/service/GroupSubService.java
@@ -3,9 +3,15 @@ package com.github.vogulev.jrtb.service;
 import com.github.vogulev.jrtb.javarushclient.dto.GroupDiscussionInfo;
 import com.github.vogulev.jrtb.repository.entity.GroupSub;
 
+import java.util.Optional;
+
 /**
  * Service for manipulating with {@link GroupSub}.
  */
 public interface GroupSubService {
     GroupSub save(Long chatId, GroupDiscussionInfo groupDiscussionInfo);
+
+    GroupSub save(GroupSub groupSub);
+
+    Optional<GroupSub> findById(Integer id);
 }
